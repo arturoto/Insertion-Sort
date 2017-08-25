87bc621c08a7235be3b1cc4ae78b8ea2ace8e536
Arturo Torres <arturoto@buffalo.edu>
2017-08-25 16:27:20 -0400

    Staring insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+
+
+public class InsertionSort{
+
+
+	/*
+	Sudo code from CS50:
+
+	for i = 1 to n - 1 
+		element = array[i]
+		j = i
+		while (j > 0 and array[j - 1] > element)
+			array[j] = array[j - 1]
+			j = j - 1
+		array[j] = elements
+
+	Sudo code 
+
+	*/
+
+	public static sortingMethod(int[] arrayToSort){
+
+
+
+	}
+
+
+
+
+}
